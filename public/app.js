ebd195da7d4677ba1cc5b3a40679e8ee5267206f
Renan <seu-email@github.com>
2026-02-18 20:22:56 +0000

    chore: publicado no Firebase Hosting

--- public/app.js
+++ public/app.js
@@ -123,7 +123,7 @@ const icmbioAgents = [
   'CONTINGÊNCIA',
 ];
 
-const scriptUrl = 'https://script.google.com/macros/s/AKfycbxr2o4TxV9C462jxVhO-iXhML8h5Mqrg3Qrjhe5dwTYK26vtUDMc0sLMBSeTBzQx8ckYw/exec';
+const scriptUrl = 'https://script.google.com/macros/s/AKfycbw9xovlmTNVycTNjDz8PIyKkEMVQZm40pb8NeBkgv3mwock9AAZh29ZnY03JksHfaFr9A/exec';
 const firebaseConfig = {
   apiKey: 'AIzaSyAh3dTQ4EeibeakaiCE5fTUpDxplrJDFK4',
   authDomain: 'veiculosnapraianao.firebaseapp.com',
